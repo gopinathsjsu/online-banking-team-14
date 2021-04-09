53bd96096ac450d94b89d7f7b6c560ad6c02784f
heli712 <72815931+heli712@users.noreply.github.com>
2021-04-09 01:49:48 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './Header'
+import Login from './Login'
+import Footer from './Footer'
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Login />
+      <Footer />
     </div>
   );
 }
